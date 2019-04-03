f1efb292268f82caffbf4255103a864e9c8c6411
Louis Slater <louismcs@hotmail.co.uk>
2019-04-03 18:31:55 +0100

    Establishing Vue/Express setup

---
+++ client/src/PostService.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const url = '/posts';
+
+class PostService {
+    //Get posts
+    static getPosts() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(data.map((post : any) => ({
+                    ...post,
+                    createdAt: new Date(post.createdAt)
+                })));
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+}
+
+export default PostService;
